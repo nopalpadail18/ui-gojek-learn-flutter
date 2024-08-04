c597f471541224dadad05634a4963d43635f348f
nopalpadail18 <nopalpadail@gmail.com>
2024-08-04 12:21:48 +0800

    Feat : fix slicing ui Goclub

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -213,11 +213,17 @@ class HomePage extends StatelessWidget {
                                 width: 40,
                                 height: 40,
                                 decoration: BoxDecoration(
-                                    color: icon.icon == 'goclub' ? Colors.white : icon.color,
+                                    color: icon.icon == 'goclub'
+                                        ? Colors.white
+                                        : icon.color,
                                     borderRadius: BorderRadius.circular(20)),
                                 child: SvgPicture.asset(
                                   'assets/logo/${icon.icon}.svg',
-                                  color: icon.icon == 'goclub' ? Colors.purple : icon.icon == 'other' ? dark2 : Colors.white,
+                                  color: icon.icon == 'goclub'
+                                      ? Colors.purple
+                                      : icon.icon == 'other'
+                                          ? dark2
+                                          : Colors.white,
                                   width: 24,
                                 ),
                               ),
@@ -231,6 +237,81 @@ class HomePage extends StatelessWidget {
                             ],
                           ),
                         ))
+                  ],
+                ),
+              ),
+            ),
+
+            // Goclub
+
+            Padding(
+              padding: const EdgeInsets.only(top: 19, left: 15, right: 15),
+              child: Container(
+                height: 65,
+                width: double.infinity,
+                clipBehavior: Clip.hardEdge,
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                      colors: [Color(0xFFEAF3F6), Colors.white]),
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.all(
+                    color: const Color(0xFFE8E8E8),
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Positioned(
+                        left: 8,
+                        top: 4,
+                        bottom: 4,
+                        child: SvgPicture.asset('assets/logo/dots.svg')),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 12),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SvgPicture.asset(
+                            'assets/logo/star.svg',
+                            height: 40,
+                          ),
+                          const SizedBox(
+                            width: 16,
+                          ),
+                          Flexible(
+                              child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text('117 XP lagi ada Harta Karun'),
+                              const SizedBox(
+                                height: 8,
+                              ),
+                              Container(
+                                  width: double.infinity,
+                                  clipBehavior: Clip.hardEdge,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(2),
+                                  ),
+                                  child: LinearProgressIndicator(
+                                    backgroundColor: dark3,
+                                    color: green1,
+                                    value: .8,
+                                  ))
+                            ],
+                          )),
+                          const SizedBox(
+                            width: 24,
+                          ),
+                          SvgPicture.asset(
+                            'assets/logo/left.svg',
+                            height: 24,
+                            color: dark1,
+                          ),
+                        ],
+                      ),
+                    )
                   ],
                 ),
               ),
